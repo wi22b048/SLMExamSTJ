4935bf0b4799290dd22b10c1ec3621428271a65a
Aleksandar Stajkovic <stj@Kendox.local>
2023-06-28 18:47:40 +0200

    Initial commit

---
+++ SLMExamSTJ/src/main/java/com/example/SLMExamSTJ/SlmExamStjApplication.java
@@ -0,0 +1,13 @@
+package com.example.SLMExamSTJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmExamStjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmExamStjApplication.class, args);
+	}
+
+}
---
+++ SLMExamSTJ/src/test/java/com/example/SLMExamSTJ/SlmExamStjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SLMExamSTJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmExamStjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
